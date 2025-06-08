499c718de0084bcfbac68faa9cc3c7b006b6a2ed
rangulo <rangulo@CR9-ITS-RANGULO>
2025-06-08 16:15:03 -0600

    Agrega scripts start.bat y run-dev.bat para facilitar ejecución

---
+++ start.bat
@@ -0,0 +1,12 @@
+@echo off 
+echo ====================================== 
+echo Instalando dependencias del proyecto... 
+echo ====================================== 
+npm install 
+ 
+echo ============================ 
+echo Iniciando servidor de Vite... 
+echo ============================ 
+npm run dev 
+ 
+pause 
