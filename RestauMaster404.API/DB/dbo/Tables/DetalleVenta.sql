9650dda0c7df509793aae0c229f165eea09b7b54
Yazmin <yrivera40037@ufide.ac.cr>
2025-07-30 23:11:00 -0600

    Se agrega cascaron del API

---
+++ RestauMaster404.API/DB/dbo/Tables/DetalleVenta.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[DetalleVenta] (
+    [idDetalleVenta] INT        IDENTITY (1, 1) NOT NULL,
+    [idVenta]        INT        NOT NULL,
+    [idPlatillo]     INT        NOT NULL,
+    [cantidad]       INT        NOT NULL,
+    [precioUnitario] FLOAT (53) NOT NULL,
+    [subTotal]       FLOAT (53) NOT NULL,
+    PRIMARY KEY CLUSTERED ([idDetalleVenta] ASC)
+);
+
