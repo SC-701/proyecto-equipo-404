bb8edb86ed98b97b67739cf7b0d246dbab11bfe3
sebasraven <ssb2841@outlook.es>
2025-08-05 23:24:58 -0600

    Esqueleto de Web y Seguridad

---
+++ RestauMaster404.Web/Abstracciones/Modelos/Venta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Abstracciones.Modelos
+{
+    public class VentaBase
+    {
+        [Required(ErrorMessage = "La propiedad fecha es requerida")]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "La propiedad hora es requerida")]
+        public TimeSpan Hora { get; set; }
+
+        [Required(ErrorMessage = "La propiedad total es requerida")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El total debe ser mayor a cero")]
+        public double Total { get; set; }
+    }
+
+    public class VentaRequest : VentaBase
+    {
+
+    }
+
+    public class VentaResponse : VentaBase
+    {
+        public Guid Id { get; set; }
+    }
+}
